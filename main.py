a19dae36e7a00b9fea074cd0ef4a229446726a12
Lam Hai Duong <kangseobi123@gmail.com>
2026-02-11 13:43:56 +0700

    authorize

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
 
 app = FastAPI()
 
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"], 
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+# -----------------------------
+
 @app.get("/")
 def read_root():
-    return {"message": "Server F-Survival dang chay o Nha Moi!"}
+    return {"message": "Server F-Survival dang chay o Nha Moi - Da mo khoa CORS!"}
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
